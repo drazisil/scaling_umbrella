ace7bb0f21b80c5a39089e8bab486b4a73d349c1
Joe Becher <me@drazisil.com>
2021-01-17 18:45:37 -0500

    Swap lxml for defusedxml

--- main.py
+++ main.py
@@ -1,27 +1,15 @@
 import argparse
-from io import FileIO
-
-from lxml import etree
+from xml.etree.ElementTree import parse
 
 parser = argparse.ArgumentParser()
 
 
-def print_hi(name):
-    print(f"Hi, {name}")
-    return name
-
-
-def load_dtd():
-    dtd = etree.DTD(FileIO("schema/coverage-04.dtd"))
-    return dtd
-
-
 def xml_from_file(file_path):
-    tree = etree.parse(file_path)
+    tree = parse(file_path)
     print(
         "{} has {} lines.".format(
             file_path,
-            len(list(tree.xpath("./packages/package/classes/class/lines/line"))),
+            len(list(tree.find("./packages/package/classes/class/lines"))),
         )
     )
     return tree
@@ -31,9 +19,8 @@ if __name__ == "__main__":
     parser.add_argument("file1", help="first file to check")
     parser.add_argument("file2", help="second file to check")
     args = parser.parse_args()
-    print_hi("PyCharm")
-    xml_def = load_dtd()
+    print("Hello, PyCharm")
     xml1 = xml_from_file(args.file1)
-    print("{} is valid: {}".format(args.file1, xml_def.validate(xml1)))
+    print("{}".format(args.file1))
     xml2 = xml_from_file(args.file2)
-    print("{} is valid: {}".format(args.file2, xml_def.validate(xml2)))
+    print("{}".format(args.file2))
--- test_main.py
+++ test_main.py
@@ -1,5 +1,9 @@
-from main import print_hi
+import defusedxml
+
+from main import xml_from_file
+
+defusedxml.defuse_stdlib()
 
 
-def test_print_hi():
-    assert print_hi("Fred") == "Fred"
+def test_xml_from_file():
+    assert xml_from_file('fixtures/coverage_1.xml')
