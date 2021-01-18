ffbcd5dc29cf7ccc9bbeefdaa1568a4150bc12d9
Joe Becher <me@drazisil.com>
2021-01-18 12:38:59 -0500

    Add test for exception on no lines found

--- scaling_umbrella/__init__.py
+++ scaling_umbrella/__init__.py
@@ -3,13 +3,14 @@ from defusedxml.ElementTree import parse
 
 def report_from_file(file_path):
     xml_tree = parse(file_path)
-    lines = list(xml_tree.find("./packages/package/classes/class/lines"))
+    lines = xml_tree.find("./packages/package/classes/class/lines")
     if not lines:
         raise Exception("Empty lines object")
+    lines_list = list(lines)
     print(
         "{} has {} lines.".format(
             file_path,
-            len(lines),
+            len(lines_list),
         )
     )
     return xml_tree
--- tests/test_main.py
+++ tests/test_main.py
@@ -3,5 +3,15 @@ from xml.etree.ElementTree import ElementTree
 from scaling_umbrella import report_from_file
 
 
-def test_xml_from_file():
-    assert report_from_file("./fixtures/coverage_1.xml").__class__ is ElementTree
+def test_report_from_file():
+    assert (
+        report_from_file("./fixtures/coverage_1.xml").__class__ is ElementTree
+    )
+
+
+def test_report_from_file_no_lines():
+    try:
+        r = report_from_file("./fixtures/coverage_no_lines.xml")
+        assert False
+    except Exception as e:
+        assert e.__str__() == "Empty lines object"
