6b6ca7614cec4aa5ef7542b7406e764bef994c01
Joe Becher <me@drazisil.com>
2021-01-17 19:54:22 -0500

    Better check for empty set

--- scaling_umbrella/__init__.py
+++ scaling_umbrella/__init__.py
@@ -11,7 +11,7 @@ def xml_from_file(file_path):
     xml_tree = parse(file_path)
     report = BaseReport()
     lines = list(xml_tree.find("./packages/package/classes/class/lines"))
-    if len(lines) == 0:
+    if not lines:
         raise Exception("Empty lines object")
     print("{} has {} lines.".format(file_path, len(lines), ))
     return xml_tree
