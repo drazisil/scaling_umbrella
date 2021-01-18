45510a848fd6e2bf86975bceb36a550d9cb4a3e0
Joe Becher <me@drazisil.com>
2021-01-18 11:35:22 -0500

    Formatted with black

--- scaling_umbrella/__init__.py
+++ scaling_umbrella/__init__.py
@@ -9,5 +9,10 @@ def report_from_file(file_path):
     lines = list(xml_tree.find("./packages/package/classes/class/lines"))
     if not lines:
         raise Exception("Empty lines object")
-    print("{} has {} lines.".format(file_path, len(lines), ))
+    print(
+        "{} has {} lines.".format(
+            file_path,
+            len(lines),
+        )
+    )
     return xml_tree
