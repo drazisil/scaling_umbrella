0e3ed2bf8d19a6a189600f84300f5f9228cef801
Joe Becher <me@drazisil.com>
2021-01-17 19:42:14 -0500

    Create class stub

---
+++ scaling_umbrella/BaseReport.py
@@ -0,0 +1,3 @@
+class BaseReport:
+    def __init__(self):
+        pass
--- scaling_umbrella/__init__.py
+++ scaling_umbrella/__init__.py
@@ -1,8 +1,11 @@
 from xml.etree.ElementTree import parse
 
+from scaling_umbrella.BaseReport import BaseReport
+
 
 def xml_from_file(file_path):
     xml_tree = parse(file_path)
+    report = BaseReport()
     lines = list(xml_tree.find("./packages/package/classes/class/lines"))
     if len(lines) == 0:
         raise Exception('Empty lines object')
